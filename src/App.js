0da65169ad84e9dbbf443d049969aba5010429bc
Naitik Garg <naitikgarg@Naitiks-MacBook-Air.local>
2025-07-11 14:06:35 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import { Signin } from "./pages/Signin.jsx"; 
 import './App.css';
+import Cinemas from "./pages/Booking.jsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<Signin />} />
+        <Route path="/Booking" element={<Cinemas />} />
       </Routes>
     </BrowserRouter>
   );
